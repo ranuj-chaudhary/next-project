bf54978dc456f70033c74b30f47be649bce7db4e
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2025-08-24 15:14:01 +0530

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+  enabled: true,
 });
 
 const nextConfig = {
